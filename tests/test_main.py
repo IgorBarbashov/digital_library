6a747796ffc39ab95e37ecb27d9ac733710483c1
Egor Kalugin <egorkavoron@gmail.com>
2025-11-01 18:06:17 +0300

    Init

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_root() -> None:
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == {"message": "Hello, world!"}
