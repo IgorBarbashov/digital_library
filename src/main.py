1ac9e05035592e2fcd85fa3f1d1dd155e92363d3
Igor Barbashov <bis200705@gmail.com>
2025-11-14 00:45:59 +0300

    #6 Add run_migrations() to FastAPI lifespan method

--- src/main.py
+++ src/main.py
@@ -1,6 +1,25 @@
+from contextlib import asynccontextmanager
+
+from alembic.config import Config
 from fastapi import FastAPI
 
-app = FastAPI()
+from alembic import command
+from src.setting import settings
+
+
+def run_migrations():
+    alembic_cfg = Config("alembic.ini")
+    alembic_cfg.set_main_option("sqlalchemy.url", settings.alembic_db_dsn)
+    command.upgrade(alembic_cfg, "head")
+
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    run_migrations()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
