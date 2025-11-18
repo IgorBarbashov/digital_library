1a05a98b7e6844bb5b1842decd929d0fd368a4f9
Igor Barbashov <bis200705@gmail.com>
2025-11-18 04:39:44 +0300

    #21 Add typing in db.py

--- src/db/db.py
+++ src/db/db.py
@@ -1,4 +1,6 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from src.setting import settings
 
@@ -6,6 +8,9 @@ engine = create_async_engine(settings.db_dsn, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def get_async_session():
+async def get_async_session() -> AsyncGenerator[AsyncSession | None]:
     async with async_session_maker() as session:
-        yield session
+        try:
+            yield session
+        finally:
+            await session.close()
