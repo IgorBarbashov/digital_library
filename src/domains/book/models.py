879bf0ac2c9b1fbee572c7fc1feff0c91c462131
Eremenko Denis <eremenko.ds@phystech.edu>
2025-11-21 16:06:56 +0300

    added many authors support

--- src/domains/book/models.py
+++ src/domains/book/models.py
@@ -2,21 +2,29 @@ from __future__ import annotations
 
 import uuid
 
-from sqlalchemy import String, ForeignKey, types
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING, List
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 
 from src.domains.common.models import Base, BaseModelMixin
+
+if TYPE_CHECKING:
+    from src.domains.genre.models import Author, Genre
+    from src.domains.common.association.author_book import AuthorBook
 
 
 class Book(Base, BaseModelMixin):
     __tablename__ = "book"
 
-    title: Mapped[str] = mapped_column(
-        String(255), nullable=False
-    )
-    author_id: Mapped[uuid.UUID] = mapped_column(
-        types.UUID, ForeignKey("author.id"), nullable=False, unique=False
-    )
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
     genre_id: Mapped[uuid.UUID] = mapped_column(
-        types.UUID, ForeignKey("genre.id"), nullable=False, unique=False
+        PG_UUID(as_uuid=True), ForeignKey("genre.id"), nullable=False, unique=False
+    )
+
+    author: Mapped["Author"] = relationship("Author", back_populates="books")
+    genre: Mapped["Genre"] = relationship("Genre", back_populates="books")
+    author_books: Mapped[List[AuthorBook]] = relationship(
+        "AuthorBook", back_populates="author"
     )
--- src/domains/book/schema.py
+++ src/domains/book/schema.py
@@ -4,7 +4,6 @@ from src.domains.common.schema import BaseSchema
 
 class BookBase(BaseSchema):
     title: str
-    author_id: uuid.UUID
     genre_id: uuid.UUID
 
 
