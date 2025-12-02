c358f20d4fca8a5f92b595f93638f333a03f1ede
Igor Barbashov <bis200705@gmail.com>
2025-12-02 06:57:21 +0000

    #25 Разработать API для работы с авторами - этап 2

--- src/domains/common/schema.py
+++ src/domains/common/schema.py
@@ -1,5 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+
+from src.exceptions.entity import NoDataToPatchEntity
 
 
 class BaseSchema(BaseModel):
     pass
+
+
+class BasePatchSchema(BaseSchema):
+    @model_validator(mode="before")
+    def check_at_least_one(cls, values):
+        if not values or all(v is None for v in values.values()):
+            raise NoDataToPatchEntity()
+        return values
