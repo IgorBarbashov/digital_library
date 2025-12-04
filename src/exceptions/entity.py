0bdf8f9d0d1a9a5d47b992d8fcf7db6571284e3e
Igor Barbashov <bis200705@gmail.com>
2025-12-04 22:27:50 +0300

    #29 Fix exception handlers

--- src/exceptions/entity.py
+++ src/exceptions/entity.py
@@ -3,17 +3,17 @@ from typing import Any
 
 class EntityNotFound(Exception):
     def __init__(self, filters: dict[str, Any], entity_name: str = "entity"):
-        self.msg = f"{entity_name.capitalize()} with filters {filters=} not found"
-        super().__init__(self.msg)
+        self.message = f"{entity_name.capitalize()} with {filters=} not found"
+        super().__init__(self.message)
 
 
 class EntityAlreadyExists(Exception):
     def __init__(self, filters: dict[str, Any], entity_name: str = "entity"):
-        self.msg = f"{entity_name.capitalize()} with filters {filters} already exists"
-        super().__init__(self.msg)
+        self.message = f"{entity_name.capitalize()} with {filters=} already exists"
+        super().__init__(self.message)
 
 
 class NoDataToPatchEntity(Exception):
     def __init__(self, entity_name: str = "entity"):
-        self.msg = f"No data to patch {entity_name.capitalize()}"
-        super().__init__(self.msg)
+        self.message = f"No data to update {entity_name.capitalize()}"
+        super().__init__(self.message)
