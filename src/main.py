01ba28adad42f9c4f5a327d068a0aee9d0bef6b4
Egor Kalugin <egorkavoron@gmail.com>
2025-12-13 14:20:53 +0300

    merge fix

--- src/main.py
+++ src/main.py
@@ -1,19 +1,10 @@
-from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from src.api.v1.init import init_routers
 from src.exceptions.init import init_exception_handlers
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    init_routers(app)
-    yield
-
-
 app = FastAPI(
-    lifespan=lifespan,
     docs_url="/api/docs",
     openapi_url="/api/openapi.json",
 )
